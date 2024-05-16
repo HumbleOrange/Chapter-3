d98cc1011597c752b2a1bd3b22f0abf2f94b3f36
HumbleOrange <1798292189@qq.com>
2024-05-16 14:23:44 +0800

    ch3ex3

--- Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -1,21 +1,36 @@
 package com.example.chapter3.homework;
 
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PlaceholderFragment extends Fragment {
-
+    private View anim;
+    private ListView text;
+    private String data[] = {"好友 1", "好友 2", "好友 3", "好友 4", "好友 5",
+                             "好友 6", "好友 7", "好友 8", "好友 9", "好友 10"};//data
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
-        return inflater.inflate(R.layout.fragment_placeholder, container, false);
+        View view = inflater.inflate(R.layout.fragment_placeholder, container, false);
+        anim = view.findViewById(R.id.animation_view);
+        text = view.findViewById(R.id.textt);
+        text.setAlpha(0);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data);//新建并配置ArrayAapeter
+        text.setAdapter(adapter);
+
+        return view;
     }
 
     @Override
@@ -27,6 +42,13 @@ public class PlaceholderFragment extends Fragment {
             public void run() {
                 // 这里会在 5s 后执行
                 // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(anim, "alpha", 1.0f, 0.0f, 0.0f);
+                alphaAnimator.setDuration(2000);
+                alphaAnimator.start();
+
+                ObjectAnimator alphaAnimator2 = ObjectAnimator.ofFloat(text, "alpha", 0.0f, 1.0f, 1.0f);
+                alphaAnimator2.setDuration(2000);
+                alphaAnimator2.start();
             }
         }, 5000);
     }
